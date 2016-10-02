f68240fc53df7791582d4246c0105ce98e12d543
ACraig28 <alexandra.craig@gmail.com>
2016-10-02 11:11:05 -0400

    temp gave up on classes. methods for add and delete table now work.

--- pokemon.rb
+++ pokemon.rb
@@ -1,22 +1,44 @@
 require 'pg'
 
 def disable_notices(conn)
-  conn.exec('SET client_min_messages TO WARNING;')
+  conn.exec('SET client_min_messages TO WARNING;') # still like some kind of msg to go out
 end
 
-def create_pokemon_table(conn, table_name)
-  conn.exec("CREATE TABLE IF NOT EXISTS #{table_name}("\
+def createTable(conn, tableName)
+  conn.exec("CREATE TABLE IF NOT EXISTS #{tableName}("\
   'id SERIAL PRIMARY KEY, name varchar, type varchar,'\
    'move varchar, rank numeric(3), effective varchar)')
+ end
+
+def alterTable
 end
 
-def remove_pokemon_table
+def removeTable(conn, deleteTable)
+   conn.exec("DROP TABLE #{deleteTable}")
 end
+
+
+
 
 def main()
 conn = PG.connect(dbname: 'pokemon')
 disable_notices(conn)
-create_pokemon_table(conn, 'water')
+puts "To create a pokemon table, type 'create', to delete a table, type 'delete'."
+response = gets.chomp
+  if response == 'create'
+    puts 'What type of pokemon table do you want to create?'
+    newTable = gets.chomp
+    createTable(conn, newTable)
+    puts "Your #{newTable} table has been created."
+  elsif response == 'delete'
+    puts "Are you sure?"
+    confirmation = gets.chomp
+    if confirmation == 'yes'
+      puts 'What table would you like to delete?'
+    archaicTable = gets.chomp
+      removeTable(conn, archaicTable)
+    end
+  end
 end
 
 main if __FILE__==$PROGRAM_NAME
